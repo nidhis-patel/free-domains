4515f8cdda0ed1e3cd0b2f6da5f24e98747a8467
nidhis-patel <nidhi.patel@acquaintsofttech.com>
2023-04-24 12:58:56 +0530

    Domain added js file

---
+++ domains/nidhis-patel.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'nidhis-patel',
+  owner: {
+    repo: 'https://github.com/nidhis-patel/nidhis-patel.github.io',
+    email: 'nidhi.patel@acquaintsofttech.com',
+  },
+  record: {
+    CNAME: 'nidhis-patel.github.io',
+  },
+  proxy: false,
+})
